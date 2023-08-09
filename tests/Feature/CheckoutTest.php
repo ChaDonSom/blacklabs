a5c5644cd5356e49439a350c2a115141a17af719
Chase Drift Ubuntu <chadonsom@gmail.com>
2023-08-09 16:33:47 -0400

    Fix checkouttest

--- tests/Feature/CheckoutTest.php
+++ tests/Feature/CheckoutTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 it('runs', function () {
     $this->artisan('checkout ' . $this->branchTwoName)
         ->expectsOutput("Checking for migrations in current branch first...")
-        ->expectsOutput("Checking out to {$this->branchTwoName}...")
-        ->expectsOutput("Done!")
+        ->expectsOutput("\nChecking out to {$this->branchTwoName}...")
+        ->expectsOutput("\nDone!")
         ->assertExitCode(0);
 });
