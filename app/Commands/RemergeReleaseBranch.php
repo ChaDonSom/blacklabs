805aa4d6f28e52a6b9a494998cfe705ab3580f33
Chase Macbook Air <chadonsom@gmail.com>
2024-01-26 13:26:10 -0500

    update remerge command to work better based off current branches

--- app/Commands/RemergeReleaseBranch.php
+++ app/Commands/RemergeReleaseBranch.php
@@ -8,19 +8,57 @@ use App\Services\RunsProcesses;
 use Illuminate\Support\Facades\Log;
 use LaravelZero\Framework\Commands\Command;
 
+use function Laravel\Prompts\search;
+
 class RemergeReleaseBranch extends Command
 {
     use RunsProcesses;
     use FindsIssueBranches;
     use MergesBranches;
 
-    protected $signature = 'merge-and-increment-tag
-                            {release-branch : The release branch to merge new changes into (required)}
-                            {issues : The issue branches to merge into the release branch (required, comma-separated)}';
+    protected $signature = 'remerge-release-branch
+                            {issues? : The issue branches to merge into the release branch (comma-separated)}
+                            {release-branch? : The release branch to merge new changes into}
+                            ';
 
-    public function handle() {
+    public function handle()
+    {
         $releaseBranch = $this->argument('release-branch');
         $issues = $this->argument('issues');
+
+        if (!$issues) {
+            // Try get the issue from the current branch
+            $this->info("No issues given, trying to get the issue from the current branch...");
+            $currentBranch = $this->runProcess('git rev-parse --abbrev-ref HEAD');
+            if ($this->isIssueBranch($currentBranch)) $issues = $this->getIssueNumberFromIssueBranch($currentBranch);
+        }
+
+        if (!$issues) {
+            $this->error("No issues given and no issue branch checked out.");
+            return;
+        }
+
+        if (!$releaseBranch) {
+            // Get the release branches that include the given issues in their names
+            $this->info("No release branch given, trying to find the release branch from the issues...");
+            $releaseBranches = $this->findReleaseBranches(explode(',', $issues));
+            print_r($releaseBranches);
+            $this->info("Found release branches: " . implode(', ', $releaseBranches));
+            if (count($releaseBranches) === 0) {
+                $this->error("No release branches found for the given issues.");
+                return;
+            } elseif (count($releaseBranches) === 1) {
+                $releaseBranch = $releaseBranches[0];
+            } elseif (count($releaseBranches) > 1) {
+                $releaseBranch = search(
+                    label: $message ?? 'What branch would you like to merge into?',
+                    options: fn (string $value) => strlen($value) > 0
+                        ? collect($releaseBranches)->filter(fn ($branch) => str_contains($branch, $value))->values()->toArray()
+                        : collect($releaseBranches)->toArray(),
+                    scroll: 10,
+                );
+            }
+        }
 
         // Find the issue branches from the issue numbers
         $issueBranches = $this->findIssueBranches(explode(',', $issues));
@@ -33,16 +71,48 @@ class RemergeReleaseBranch extends Command
         // Increment the tag using npm version prerelease, then get the new tag
         $this->runProcess("npm version prerelease");
 
-        $newTag = $this->runProcess("git describe --tags --abbrev=0"); // v0.15.0-2
-
-        $this->info("New tag: {$newTag}");
-        Log::debug("New tag: " . $newTag);
-
         // Push the branch and the tags
         $this->info("Pushing the branch and the tag...");
-        $this->runProcess("git push origin {$releaseBranch}");
-        $this->runProcess("git push origin {$newTag}");
+        $this->runProcess("git push origin {$releaseBranch} --follow-tags");
 
         $this->info("Done!");
+    }
+
+    /**
+     * Determine whether the given branch is an issue branch. Issue branches are named {issue number}-{issue title slug}.
+     */
+    private function isIssueBranch(string $branchName): bool
+    {
+        return preg_match('/^\d+-/', $branchName);
+    }
+
+    /**
+     * Get the issue number from the given issue branch name.
+     */
+    private function getIssueNumberFromIssueBranch(string $branchName): string
+    {
+        return explode('-', $branchName)[0];
+    }
+
+    /**
+     * Get the release branches that include the given issues in their names. Release branches are formatted like
+     * release/{version}/{issue numbers, kebab-case}.
+     */
+    private function findReleaseBranches(array $issues): array
+    {
+        $releaseBranches = [];
+        foreach ($issues as $issue) {
+            // Find all release branches that have this issue's number anywhere in their name
+            $thisIssuesBranches = $this->runProcess(
+                "git branch --list --remote 'origin/release/*/*{$issue}*'"
+            );
+            print_r('thisIssuesBranches: ');
+            print_r($thisIssuesBranches);
+            $releaseBranches = array_merge($releaseBranches, array_filter(
+                explode("\n", $thisIssuesBranches),
+                fn ($branch) => strlen($branch) > 0
+            ));
+        }
+        return $releaseBranches;
     }
 }
--- tests/Feature/RemergeReleaseBranchTest.php
+++ tests/Feature/RemergeReleaseBranchTest.php
@@ -5,10 +5,9 @@ use Illuminate\Support\Str;
 it('merges issue branches and increments the tag', function () {
     $this->artisan('create-release-branch premajor 123,456');
     $branchTwoNameLimited = Str::limit($this->branchTwoName, 20, '...');
-    $this->artisan('merge-and-increment-tag release/v2.0.0-0/123-456 456')
+    $this->artisan('remerge-release-branch 456 release/v2.0.0-0/123-456')
         ->expectsOutput('Finding branch for issue 456...')
         ->expectsOutput('Merging ' . $branchTwoNameLimited . '...')
         ->expectsOutput('Pushing the branch and the tag...')
         ->expectsOutput('Done!');
 })->group('dummy-git-repo');
-
